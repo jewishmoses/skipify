0cf09116bc0f53d786a7c9c90b5fe4b600f9f532
jewishmoses <work@xmoses.com>
2021-12-16 11:22:26 +0200

    app.js & background.s -> server.js

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+require('dotenv').config();
 const axios = require('axios').default;
+const express = require('express');
+const cors = require("cors");
 const { Sequelize, Model, DataTypes } = require('sequelize');
 
 const sequelizeOptions = {
@@ -98,5 +101,28 @@ async function removeExpiredTokens() {
 
 }
 
-console.log('backogrund.js is running');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', async (req, res) => {
+
+    const movie = await Movie.findOne({ where: { active: true } });
+    const movieExists = movie != null;
+
+    if (movieExists == false) {
+        
+        res.json({ status: 'failed' });
+        return;
+
+    }
+
+    await movie.destroy();
+    res.json(movie);
+
+});
+
+app.listen(process.env.PORT, '0.0.0.0');
+
 saveNewTokenAndCookieTODB();
